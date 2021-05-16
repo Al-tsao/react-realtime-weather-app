d7d07a24a6116ad89ed5707e7691e5b051a7985d
AlTsao <al.tsao.yw@gmail.com>
2021-05-16 15:15:04 +0800

    install normalize.css

--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ import App from './App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
 
+import 'normalize.css';
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
